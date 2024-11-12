efa247711702b3de38598718651ca19bbd8aea86
d_malp <mehmet.alp@ginitalent.com>
2024-11-12 10:53:44 +0300

    reinit repo

--- src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of report-from-website
- */
-
-
-export * from './lib/report-button.component';
-export * from './lib/report-from-website.module';
-export * from './lib/report-from-website.component';
-export * from './lib/report-from-website.service';
